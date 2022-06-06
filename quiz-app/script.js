9a5d177864b215c848df8b8bb395c2d8846c2321
Renven <renven666@gmail.com>
2022-06-07 00:00:39 +0800

    [part of quiz-app];

---
+++ quiz-app/script.js
@@ -0,0 +1,39 @@
+const quizData = [
+    {
+        question: 'What old is Renven?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2022?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    }, {
+        question: 'Who is the author of 《Big Debt Crises》>? ',
+        a: 'Wilson Chip',
+        b: 'Ray Dalio',
+        c: 'Ivan Saldano',
+        d: 'Howard Schultz',
+        correct: 'b'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a'
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'd'
+    }
+
+]
